fe21c5f986f4ccd4b8924fd2db1ce15b0bc2b320
Kyle <notxap55@yahoo.com>
2024-08-29 14:42:15 -0700

    project completed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from io import text_encoding
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,25 +11,56 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+timer = ''
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    title_label.config(text='Title', fg=GREEN)
+    canvas.itemconfig(timer_text, text='00:00')
+    checkmark_label.config(text='')
+    global reps
+    reps = 0
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text='Break', fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text='Break', fg=PINK)
+    else:
+        count_down(work_sec)
+        title_label.config(text='Work', fg=GREEN)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-def count_down(seconds):
-    minutes = seconds // 60
-    remaining_seconds = seconds % 60
-    canvas.itemconfig(timer_text, text=f"{minutes:02}:{remaining_seconds:02}")
-    window.after(1000, count_down, seconds - 1)
+def count_down(count):
+    count_min = count // 60
+    count_sec = count % 60
+    canvas.itemconfig(timer_text, text=f"{count_min:02}:{count_sec:02}")
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ''
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += '✔'
+        checkmark_label.config(text=marks)
+    return timer
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
-
 
 title_label = Label(text="Timer", font=(FONT_NAME, 40, 'bold'), bg=YELLOW, fg=GREEN)
 title_label.grid(column=1, row=0)
@@ -38,13 +71,13 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 143, text='00:00', fill='white', font=(FONT_NAME, 35, "bold italic"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text='Start', command=start_timer, bg='white', highlightthickness=0)
+start_button = Button(command=start_timer, text='Start', bg='white', highlightthickness=0)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text='Reset', bg='white', highlightthickness=0)
+reset_button = Button(command=reset_timer, text='Reset', bg='white', highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
-checkmark_label = Label(text='✔', font=(FONT_NAME, 24), bg=YELLOW, fg=GREEN)
+checkmark_label = Label(font=(FONT_NAME, 24), bg=YELLOW, fg=GREEN)
 checkmark_label.grid(column=1, row=3)
 
 
